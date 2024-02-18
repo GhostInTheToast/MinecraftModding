6b42133f0cab93d8fc5898bd35fd508da0d9fc16
Bilal Mahmood <bmood1@gmail.com>
2024-02-18 16:58:38 -0600

    import com.mojang.logging.LogUtils;

--- src/main/java/ghostinthetoast/minecraftmodding/MinecraftModding.java
+++ src/main/java/ghostinthetoast/minecraftmodding/MinecraftModding.java
@@ -1,10 +1,11 @@
 package ghostinthetoast.minecraftmodding;
 
-// import com.mojang.logging.LogUtils;
+import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +15,6 @@ import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.stream.Collectors;
 
@@ -23,15 +23,16 @@ import java.util.stream.Collectors;
 public class MinecraftModding {
 
     // Directly reference a slf4j logger
-    public static final Logger logger = LoggerFactory.getLogger(MinecraftModding.class);
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public MinecraftModding() {
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        bus.addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        bus.addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
