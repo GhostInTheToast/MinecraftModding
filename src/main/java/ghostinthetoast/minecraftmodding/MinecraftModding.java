f088978a7999dad99afb231e333662e11a5c907c
Bilal Mahmood <bmood1@gmail.com>
2024-02-18 17:48:07 -0600

    logger class replaced by log4j variant to fix issues.

--- src/main/java/ghostinthetoast/minecraftmodding/MinecraftModding.java
+++ src/main/java/ghostinthetoast/minecraftmodding/MinecraftModding.java
@@ -1,6 +1,5 @@
 package ghostinthetoast.minecraftmodding;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -14,7 +13,8 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
@@ -23,15 +23,12 @@ import java.util.stream.Collectors;
 public class MinecraftModding {
 
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public MinecraftModding() {
-        // Register the setup method for modloading
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::setup);
-        // Register the enqueueIMC method for modloading
         bus.addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
         bus.addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
